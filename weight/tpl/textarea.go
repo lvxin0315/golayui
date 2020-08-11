8c01e9006be52ba01a7c341f8aed7c8f51ec15f5
lvxin <lvxin0315@163.com>
2020-08-11 14:20:30 +0800

    textarea 添加内容

--- weight/tpl/textarea.go
+++ weight/tpl/textarea.go
@@ -1,7 +1,3 @@
 package tpl
 
-const TextareaTpl = `<textarea placeholder="{{.Placeholder}}" name="{{.Name}}" id="{{.Id}}" class="layui-textarea">
-{{if .Content}}
-{{.Content}}
-{{end}}
-</textarea>`
+const TextareaTpl = `<textarea placeholder="{{.Placeholder}}" name="{{.Name}}" id="{{.Id}}" class="layui-textarea">{{if .Content}}{{.Content}}{{end}}</textarea>`
